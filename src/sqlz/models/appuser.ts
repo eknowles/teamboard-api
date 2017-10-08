b4c29914500e7e7d874bce06bf7d252954dbabc8
Edward Knowles <mail@eknowles.com>
2017-10-08 14:00:35 +0100

    chore(app): init

---
+++ src/sqlz/models/appuser.ts
@@ -0,0 +1,42 @@
+import * as Sequelize from 'sequelize';
+
+export interface AppUserAttributes {
+  id?: string;
+  active?: boolean;
+  avatar?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  notification?: boolean;
+  phone?: string;
+  pwd?: string;
+  languageId?: string;
+}
+
+export interface AppUserInstance extends Sequelize.Instance<AppUserAttributes> {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  email: string;
+  pwd: string;
+  languageId: string;
+}
+
+export default function defineUser(sequelize: Sequelize.Sequelize, DataTypes) {
+  const AppUser = sequelize.define('AppUser', {
+    email: DataTypes.STRING,
+    pwd: DataTypes.STRING
+  }, {
+    timestamps: true,
+    classMethods: {
+      associate: function(models) {
+        AppUser.belongsTo(models.Language, {
+          foreignKey: 'languageId',
+          onDelete: 'CASCADE',
+        });
+      }
+    }
+  });
+  return AppUser;
+}
